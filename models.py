40f5ed28d291439b7576745b71f0f1ee1675b890
Michael Michaelshvili <michaelm748@gmail.com>
2021-05-07 14:15:40 +0300

    Initial commit

---
+++ models.py
@@ -0,0 +1,79 @@
+import numpy as np
+from keras.layers import LSTM, Dense, concatenate, Input, Embedding, Dropout, BatchNormalization, TimeDistributed, \
+    Flatten, Activation, dot
+from keras.models import Model, Sequential
+from keras.optimizers import Adagrad
+from keras.losses import CategoricalCrossentropy
+import pickle as pkl
+import data_preproccesing as dp
+
+
+def build_model_1(embedding_w):
+    """
+    build first model
+    :param embedding_w: embedding weights from the fasttest pretrain model
+    :return:
+    """
+    lyrics_input = Input(shape=(1,), name='lyrics_input')
+    midi_input = Input(shape=(297,), name='midi_input')
+    lyrics_embedding_l = Embedding(embedding_w.shape[0], 300, weights=[embedding_w], input_length=1, trainable=False,
+                                   name='lyrics_embedding')(lyrics_input)
+    lstm_1 = LSTM(100)(lyrics_embedding_l)
+    concat_l = concatenate([lstm_1, midi_input])
+    dense_1 = Dense(2024)(concat_l)
+    bn_l = BatchNormalization()(dense_1)
+    dense_2 = Dense(1024)(bn_l)
+    dropout_l = Dropout(0.1)(dense_2)
+    output_l = Dense(embedding_w.shape[0], activation='softmax')(dropout_l)
+    model = Model([lyrics_input, midi_input], output_l)
+    model.compile(optimizer=Adagrad(), loss=CategoricalCrossentropy(), metrics=['acc'])
+    return model
+
+
+def midi_autoencoder():
+    """
+    biuld autoencoder for create new representation for midi vector
+    :return:
+    """
+    encoder_input = Input(shape=(297,))
+    d1 = Dense(297, activation='relu')(encoder_input)
+    d2 = Dense(150, activation='relu')(d1)
+    d3 = Dense(75, activation='relu')(d2)
+    d4 = Dense(40, activation='relu')(d3)
+    d5 = Dense(75, activation='relu')(d4)
+    d6 = Dense(150, activation='relu')(d5)
+    d7 = Dense(297, activation='relu')(d6)
+
+    autoencoder = Model(encoder_input, d7)
+    encoder = Model(encoder_input, d4)
+
+    autoencoder.compile(optimizer=Adagrad(), loss='mse')
+
+    return autoencoder, encoder
+
+
+def build_model_2(embedding_w):
+    """
+    Build the second model
+    :param embedding_w: embedding weights from the fasttest pretrain model
+    :return:
+    """
+    lyrics_input = Input(shape=(1,), name='lyrics_input')
+    midi_input = Input(shape=(40,), name='midi_input')
+    lyrics_embedding_l = Embedding(embedding_w.shape[0], 300, weights=[embedding_w], input_length=1, trainable=False,
+                                   name='lyrics_embedding')(lyrics_input)
+    lstm_1 = LSTM(200, return_sequences=True)(lyrics_embedding_l)
+    attention = TimeDistributed(Dense(1, activation='tanh'))(lstm_1)
+    attention = Flatten(name='attention_flatten')(attention)
+    attention = Activation('softmax')(attention)
+    attention = dot([lstm_1, attention], axes=[1, 1], name='dot_attention')
+
+    concat_l = concatenate([attention, midi_input])
+    dense_1 = Dense(2024)(concat_l)
+    bn_l = BatchNormalization()(dense_1)
+    dense_2 = Dense(1024)(bn_l)
+    dropout_l = Dropout(0.1)(dense_2)
+    output_l = Dense(embedding_w.shape[0], activation='softmax')(dropout_l)
+    model = Model([lyrics_input, midi_input], output_l)
+    model.compile(optimizer=Adagrad(), loss=CategoricalCrossentropy(), metrics=['acc'])
+    return model
---
+++ train_and_evaluate.py
@@ -0,0 +1,145 @@
+import models as m
+import pickle as pkl
+import data_preproccesing as dp
+import numpy as np
+import pandas as pd
+import os
+from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, TensorBoard
+from keras.models import load_model
+
+
+def train_model_1():
+    """
+    train the first model
+    :return:
+    """
+    vocabulary_size = dp.get_vocabulary_size()
+    x, one_hot_y, instance_to_song, song_indexes, word_indexer = dp.prepare_set(vocabulary_size)
+    midi_data_dict = pkl.load(open('midi_vectors.pkl', 'rb'))  # dp.get_midi_vectors()
+    x_lyrics_train, x_melody_train, x_lyrics_val, x_melody_val, y_train, y_val = dp.split_train_validation(x, one_hot_y,
+                                                                                                           instance_to_song,
+                                                                                                           midi_data_dict)
+    embadding = pkl.load(open('emb_w.pkl', 'rb'))
+    lyrics_model = m.build_model_1(embadding)
+    rlop = ReduceLROnPlateau(min_delta=0.01)
+    mcp = ModelCheckpoint('model_checkpoint.h5', save_best_only=True)
+    tensorboard_callback = TensorBoard(log_dir="logs", histogram_freq=1)
+    history = lyrics_model.fit([np.array(x_lyrics_train), np.array(x_melody_train)], np.array(y_train), epochs=100,
+                               batch_size=512,
+                               validation_data=([np.array(x_lyrics_val), np.array(x_melody_val)], np.array(y_val)),
+                               verbose=1, callbacks=[rlop, mcp, tensorboard_callback])
+    lyrics_model.save('model_1.h5')
+    return history, word_indexer
+
+
+def train_model_2():
+    """
+    train the second model
+    :return:
+    """
+    vocabulary_size = dp.get_vocabulary_size()
+    x, one_hot_y, instance_to_song, song_indexes, word_indexer = dp.prepare_set(vocabulary_size)
+    midi_data_dict = pkl.load(open('encoded_midi_vectors', 'rb'))  # dp.get_midi_vectors()
+    x_lyrics_train, x_melody_train, x_lyrics_val, x_melody_val, y_train, y_val = dp.split_train_validation(x, one_hot_y,
+                                                                                                           instance_to_song,
+                                                                                                           midi_data_dict)
+    embadding = pkl.load(open('emb_w.pkl', 'rb'))
+    lyrics_model = m.build_model_2(embadding)
+    rlop = ReduceLROnPlateau(min_delta=0.01)
+    mcp = ModelCheckpoint('model2_checkpoint.h5', save_best_only=True)
+    print('start fit')
+    tensorboard_callback = TensorBoard(log_dir="logs", histogram_freq=1)
+
+    history = lyrics_model.fit([np.array(x_lyrics_train), np.array(x_melody_train)], np.array(y_train), epochs=30,
+                               batch_size=8192,
+                               validation_data=([np.array(x_lyrics_val), np.array(x_melody_val)], np.array(y_val)),
+                               verbose=1, callbacks=[rlop, mcp, tensorboard_callback])
+    lyrics_model.save('model_2.h5')
+    return history, word_indexer
+
+
+def generate_word(lyrics_gen_model, prev_word, midi_vec, word_idx):
+    """
+    generate next word for song. the choose is not determenistic.
+    :param lyrics_gen_model: model that generate words
+    :param prev_word: the previous word in the song
+    :param midi_vec: the midi representation of the song
+    :param word_idx: dictionary of all words.
+    :return:
+    """
+    word_emb = np.array([word_idx[prev_word]])
+    pred_words = lyrics_gen_model.predict([[word_emb], [midi_vec]])
+    # the next lins choose word non determenistic. the probability is depend on the model's prediction
+    acc_pred_vec = np.add.accumulate(pred_words, axis=1)
+    rnd = np.random.random_sample()
+    l_acc = len(acc_pred_vec)
+    l_rnd = len(acc_pred_vec[acc_pred_vec > rnd])
+    predicted_word = list(word_idx.keys())[l_acc - l_rnd - 1]
+    return predicted_word
+
+
+def train_autoencoder():
+    """
+    train autoencoder model
+    :return:
+    """
+    midi_data_dict = pkl.load(open('midi_vectors.pkl', 'rb'))
+    x_train = []
+    x_val = []
+    for key in midi_data_dict.keys():
+        if np.random.random_sample() <= 0.2:
+            x_val.append(midi_data_dict[key])
+        else:
+            x_train.append(midi_data_dict[key])
+    x_train = np.array(x_train)
+    x_val = np.array(x_val)
+    ae_model = m.midi_autoencoder()
+    ae_model[0].fit(x_train, x_train, validation_data=(x_val, x_val), epochs=20, batch_size=1)
+    new_midi_dict = {}
+    for key in midi_data_dict.keys():
+        new_midi_dict[key] = ae_model[1].predict(np.array([midi_data_dict[key]]))[0]
+    pkl.dump(new_midi_dict, open('encoded_midi_vectors', 'wb'))
+    ae_model[1].save('autoencoder.h5')
+
+
+def generate_test(model, word_indexer, song_length, test_path, use_autoencoder=False):
+    """
+    This function generate lyrics for all test songs
+    :param model: the model that predict the lyrics
+    :param word_indexer: dictionary for all words
+    :param song_length: how many words to generate
+    :param test_path: path of test file
+    :param use_autoencoder: if use the shrink representation of midi vector
+    :return:
+    """
+    autoencoder = load_model('autoencoder.h5')
+    initial_words = ['hello', 'i', 'always']
+    test_df = pd.read_csv(test_path, header=None)
+    song_names = []
+    for idx, row in test_df.iterrows():
+        song_names.append(row[1])
+        midi_vector = dp.midi_representation(os.path.join('midi_files', (str(row[0]) + ' - ' + str(row[1]) + '.mid')).replace(' ', '_'))
+        if use_autoencoder:
+            midi_vector = autoencoder.predict(np.array([midi_vector]))[0]
+        print(f'song - {row[1]}')
+        for word in initial_words:
+            lyrics = []
+            curr_word = word
+            for i in range(song_length):
+                curr_word = generate_word(model, curr_word, midi_vector, word_indexer)
+                lyrics.append(curr_word)
+            print(f'start word - {word}')
+            print(' '.join(lyrics).replace('eos','\n'))
+        print('******************************')
+    print(song_names)
+
+
+
+if __name__ == '__main__':
+    train_model_1()
+    # train_model_2()
+    # train_autoencoder()
+    model = load_model('model2_checkpoint.h5')
+    vocabulary_size = dp.get_vocabulary_size()
+    x, one_hot_y, instance_to_song, song_indexes, word_indexer = dp.prepare_set(vocabulary_size)
+    generate_test(model, word_indexer, 50, r"C:\Users\micha\Documents\michael\שנה ד\סמסטר ח\למידה עמוקה\Assignment3\lyrics_test_set.csv", True)
